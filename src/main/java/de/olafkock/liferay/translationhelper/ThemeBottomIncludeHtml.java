e9bed6d5837b1eb8a519cc75cad0b0efa23ab7b7
Olaf Kock <olaf.kock+git@liferay.com>
2019-12-15 14:05:38 +0100

    sort suspicious entries (no or duplicated translation) to top of HTML

--- src/main/java/de/olafkock/liferay/translationhelper/ThemeBottomIncludeHtml.java
+++ src/main/java/de/olafkock/liferay/translationhelper/ThemeBottomIncludeHtml.java
@@ -24,28 +24,41 @@ public class ThemeBottomIncludeHtml extends BaseDynamicInclude {
 			HttpServletRequest request, HttpServletResponse response,
 			String dynamicIncludeKey) throws IOException {
 		HashMap<String,HashSet<String>> result = TranslationHelperThreadLocal.retrieve();
+		StringBuffer suspicious = new StringBuffer();
+		StringBuffer unsuspicious = new StringBuffer();
+		for (String key : result.keySet()) {
+			StringBuffer thisLine = new StringBuffer();
+			StringBuffer target = unsuspicious;
+			thisLine.append("<tr>\n  <td style=\"vertical-align:top;\">\n    " + HtmlUtil.escape(key) + "\n  </td>\n  <td>\n    <ul>\n");
+			for (Iterator<String> iterator = result.get(key).iterator(); iterator.hasNext();) {
+				String value = iterator.next();
+				if(value == null) {
+					thisLine.append("      <li class=\"translationhelper-unresolved\"><b><i>null</i></b></li>\n");
+					target = suspicious;
+				} else if(value.equals("")) {
+					thisLine.append("      <li class=\"translationhelper-unresolved\"><b><i>empty</i></b></li>\n");
+					target = suspicious;
+				} else if(value.equals(key)){
+					thisLine.append("      <li class=\"translationhelper-unresolved\">");
+					thisLine.append(HtmlUtil.escape(value));
+					thisLine.append("</li>\n");
+					target = suspicious;
+				} else {
+					thisLine.append("      <li class=\"translationhelper-resolved\">");
+					thisLine.append(HtmlUtil.escape(value));
+					thisLine.append("</li>\n");
+				}
+			}
+			thisLine.append( "    </ul>\n  </td>\n</tr>\n");
+			target.append(thisLine.toString());
+		}
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println("<div id=\"translation-helper-table-result\">");
 		printWriter.println("<style>.translationhelper-unresolved { background-color:red; }</style>");
 		printWriter.println("<h1>Translations on this page</h1>");
 		printWriter.println("<table border=\"1\" style=\"vertical-align:top;\">");
-		for (String key : result.keySet()) {
-			printWriter.print("<tr>\n  <td style=\"vertical-align:top;\">\n    " + HtmlUtil.escape(key) + "\n  </td>\n  <td>\n    <ul>\n");
-			for (Iterator<String> iterator = result.get(key).iterator(); iterator.hasNext();) {
-				String value = iterator.next();
-				if(value == null) {
-					printWriter.println("      <li class=\"translationhelper-unresolved\"><b><i>null</i></b></li>");
-				} else if(value.equals("")) {
-					printWriter.println("      <li class=\"translationhelper-unresolved\"><b><i>empty</i></b></li>");
-				} else {
-					String clazz = value.equals(key) ? "translationhelper-unresolved" : "translationhelper-resolved";
-					printWriter.print("      <li class=\"" + clazz + "\">");
-					printWriter.print(HtmlUtil.escape(value));
-					printWriter.println("</li>");
-				}
-			}
-			printWriter.println( "    </ul>\n  </td>\n</tr>");
-		}
+		printWriter.println(suspicious);
+		printWriter.println(unsuspicious);
 		printWriter.println("</table>");
 		printWriter.println("</div>");
 		printWriter.println("<script>"
